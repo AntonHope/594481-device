891f493e967b33df040d3804e26b2965d972249a
Anton Hope <dock.seo@gmail.com>
2018-04-01 00:35:43 +0300

    Добавил js для модалок

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var linkWrite = document.querySelector(".modal-popup-write");
+var popupWrite = document.querySelector(".fixed-overlay");
+var closePopup = document.querySelector(".close-popup");
+var popupMap = document.querySelector(".fixed-overlay-map");
+var linkMap = document.querySelector(".modal-map")
+var closeMap = document.querySelector(".close-popup-map")
+
+linkWrite.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popupWrite.classList.add("modal-show");
+});
+
+closePopup.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popupWrite.classList.remove("modal-show");
+	});
+
+linkMap.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popupMap.classList.add("modal-show")
+});
+
+closeMap.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popupMap.classList.remove("modal-show");
+});
