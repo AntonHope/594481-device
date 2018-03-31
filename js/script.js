5fd9ca9103958a1f498ff184dfeb2eabc3090d65
Anton Hope <dock.seo@gmail.com>
2018-04-01 02:19:30 +0300

    Доделал js и внес финальные правки

--- js/script.js
+++ js/script.js
@@ -4,23 +4,26 @@ var closePopup = document.querySelector(".close-popup");
 var popupMap = document.querySelector(".fixed-overlay-map");
 var linkMap = document.querySelector(".modal-map")
 var closeMap = document.querySelector(".close-popup-map")
+var login = popupWrite.querySelector("[name=login]");
 
 linkWrite.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	popupWrite.classList.add("modal-show");
+  evt.preventDefault();
+  popupWrite.classList.add("modal-show");
+  login.focus();
 });
 
 closePopup.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	popupWrite.classList.remove("modal-show");
-	});
+  evt.preventDefault();
+  popupWrite.classList.remove("modal-show");
+  });
 
 linkMap.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	popupMap.classList.add("modal-show")
+  evt.preventDefault();
+  popupMap.classList.add("modal-show")
 });
 
 closeMap.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	popupMap.classList.remove("modal-show");
+  evt.preventDefault();
+  popupMap.classList.remove("modal-show");
 });
+
