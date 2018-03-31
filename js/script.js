206ee491dfe76eb4f58356b79613819c7be4c605
Anton Hope <dock.seo@gmail.com>
2018-04-01 02:27:21 +0300

    Добавил блокировку отправки при отсутсвии значений

--- js/script.js
+++ js/script.js
@@ -2,9 +2,12 @@ var linkWrite = document.querySelector(".modal-popup-write");
 var popupWrite = document.querySelector(".fixed-overlay");
 var closePopup = document.querySelector(".close-popup");
 var popupMap = document.querySelector(".fixed-overlay-map");
-var linkMap = document.querySelector(".modal-map")
-var closeMap = document.querySelector(".close-popup-map")
+var linkMap = document.querySelector(".modal-map");
+var closeMap = document.querySelector(".close-popup-map");
+var form = popupWrite.querySelector("form");
 var login = popupWrite.querySelector("[name=login]");
+var email = form.querySelector("[name=email]");
+var textarea = form.querySelector("textarea");
 
 linkWrite.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -26,4 +29,10 @@ closeMap.addEventListener("click", function (evt) {
   evt.preventDefault();
   popupMap.classList.remove("modal-show");
 });
+
+form.addEventListener("submit", function (evt) {
+  if (!login.value || !email.value || !textarea.value) {
+    evt.preventDefault();
+  }
+  });
 
